2de08cb9c1d5caaa849ef1fbd812586f00759ef7
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-07-14 18:20:44 +0530

    used AccountDetails to set name and fetched the name.

--- src/main/java/com/demo/app/Main.java
+++ src/main/java/com/demo/app/Main.java
@@ -2,17 +2,13 @@ package com.demo.app;
 
 import com.demo.app.Bank;
 import com.demo.app.Bank.AccountDetails;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
-
+        AccountDetails accountDetails= AccountDetails.newBuilder().setName("Hari").build();
+        System.out.println(accountDetails.getName());
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
