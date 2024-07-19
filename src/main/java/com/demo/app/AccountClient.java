0de346685717ad949a0dfb76d8b406d36ddab44b
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-07-19 17:57:05 +0530

    Added CreateAccount Service on AccountServiceImpl and createAccount request in the AccountClient.

--- src/main/java/com/demo/app/AccountClient.java
+++ src/main/java/com/demo/app/AccountClient.java
@@ -1,6 +1,7 @@
 package com.demo.app;
 import com.demo.app.Bank.AccountDetails;
 import com.demo.app.Bank.AccountRequest;
+import com.demo.app.Bank.CreateAccountRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -18,6 +19,19 @@ public class AccountClient {
         System.out.println("Account Balance: "+response.getBalance());
     }
 
+    //CreateAccount request
+    public void createAccount(String name, float initialBalance){
+        CreateAccountRequest request=CreateAccountRequest.newBuilder()
+                .setName(name)
+                .setInitialBalance(initialBalance)
+                .build();
+
+        AccountDetails response=blockingStub.createAccount(request);
+        System.out.println("Created Account Number: "+response.getAccountnumber());
+        System.out.println("Account name: "+response.getName());
+        System.out.println("Account Balance: "+response.getBalance());
+    }
+
     public static void main(String[] args){
         ManagedChannel channel=ManagedChannelBuilder.forAddress("localhost",50051)
                 .usePlaintext()
@@ -26,6 +40,8 @@ public class AccountClient {
         AccountClient client=new AccountClient(channel);
         client.getAccountDetails(12345);
 
+        //Create new Account
+        client.createAccount("Prasath",200060.0f);
         channel.shutdown();
     }
 }
--- src/main/java/com/demo/app/AccountServiceImpl.java
+++ src/main/java/com/demo/app/AccountServiceImpl.java
@@ -1,12 +1,17 @@
 package com.demo.app;
 import com.demo.app.Bank.AccountDetails;
 import com.demo.app.Bank.AccountRequest;
+import com.demo.app.Bank.CreateAccountRequest;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AccountServiceImpl extends AccountServiceGrpc.AccountServiceImplBase{
+    private final AtomicInteger accountNumGenerator=new AtomicInteger((1000));
+
+    //Get AccountDetails Service
     @Override
     public void getAccountDetails(AccountRequest request, StreamObserver<AccountDetails> responseObserver){
         int accountNumber=request.getAccountnumber();
@@ -20,6 +25,25 @@ public class AccountServiceImpl extends AccountServiceGrpc.AccountServiceImplBas
         responseObserver.onNext(accountDetails);
         responseObserver.onCompleted();
     }
+
+    //CreateAccount Service
+    @Override
+    public void createAccount(CreateAccountRequest request,StreamObserver<AccountDetails> responseObserver){
+        int accountNumber=accountNumGenerator.incrementAndGet();
+        String name=request.getName();
+        float initialBalance=request.getInitialBalance();
+
+        AccountDetails accountDetails=AccountDetails.newBuilder()
+                .setAccountnumber(accountNumber)
+                .setName(name)
+                .setBalance(initialBalance)
+                .build();
+
+        responseObserver.onNext(accountDetails);
+        responseObserver.onCompleted();
+    }
+
+
     public static void main(String[] args)throws IOException, InterruptedException{
         Server server=ServerBuilder.forPort(50051)
                 .addService(new AccountServiceImpl())
